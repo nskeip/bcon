6d4def83f9fd22df7f035325d288d74ab15efe82
Nikita Hismatov <hismatovns@gmail.com>
2011-12-15 19:59:21 +0600

    Basic block, creation from config + test

---
+++ bcon.py
@@ -0,0 +1,24 @@
+#-*- coding: UTF-8 -*-
+
+
+class Block(object):
+    def __init__(self, name):
+        self.name = name
+        self.children = {}
+
+    @classmethod
+    def create_from_config(cls, config):
+        """
+        >>> conf = { 'block': 'index', 'content': { 'header': {'block': 'header'}, 'body': {'block': 'body'}, }}
+        >>> b = Block.create_from_config(conf)
+        >>> b.name
+        'index'
+        """
+        ret = Block(config['block'])
+        
+        if 'content' in config:
+            for child_name, child_config in config['content'].iteritems():
+                ret.children[child_name] = cls.create_from_config(child_config)
+
+        return ret
+
