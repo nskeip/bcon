4ee0164dcb2f634897a158e90a1a26773284581d
Nikita Hismatov <hismatovns@gmail.com>
2012-05-23 23:28:43 +0600

    Config format created - the hardest part.

--- bcon.py
+++ bcon.py
@@ -1,38 +1,47 @@
 #-*- coding: UTF-8 -*-
 
-"""
-[
-    [ # row began
-        { # span bagan
-            'class': 'span6',
-            'content': 'some_block',
-        },
-        {
-            'class': 'span6',
-        },
-    ]
-]
-"""
 
 class Page(object):
 
     @classmethod
-    def from_config(cls):
+    def from_config(cls, config):
         """
-        >>> conf = [
-        ...     'container',
-        ...     [
-        ...         'row',
-        ...         {'class': 'span12', 'block': ['header']}
-        ...     ],
+        >>> conf = {
+        ...     'class': 'container',
+        ...     'contents': [
+        ...         {
+        ...             'class': 'row',
+        ...             'contents': [
+        ...                 {
+        ...                     'class': 'span12',
+        ...                     'contents': [
+        ...                         {'block': 'header'},
+        ...                     ],
+        ...                 }
+        ...             ]
+        ...         },
         ...
-        ...     [
-        ...         'row',
-        ...         {'class': 'span4', 'block': ['content']},
-        ...         {'class': 'span8', 'block': ['nav']},
+        ...         {
+        ...             'class': 'row',
+        ...             'contents': [
+        ...                 {
+        ...                     'class': 'span4', 
+        ...                     'contents': [{'block': 'content'}]
+        ...                 },
+        ...
+        ...                 {
+        ...                     'class': 'span8', 
+        ...                     'contents': [
+        ...                         {'block': 'nav'},
+        ...                         {'block': 'adv'},
+        ...                     ]
+        ...                 },
+        ...             ]
+        ...         }
         ...     ]
-        ... ]
+        ... }
         """
+        pass
 
 if __name__ == '__main__':
     import doctest
