3d696d04fb041119bf42f8c38c825b7df1d193f7
Nikita Hismatov <hismatovns@gmail.com>
2012-05-30 23:34:51 +0600

    Block and Wrapper. create_from_decl

--- bcon.py
+++ bcon.py
@@ -11,11 +11,30 @@
     ('row',
         ('span12', {'block': 'footer'})))
 
-
 >>> decl1 = ('container',)
 >>> w1 = Wrapper.create_from_decl(decl1)
 >>> w1.class_
 'container'
+>>> w1.blocks
+[]
+
+>>> decl2 = ('span12', {'block': 'nav'})
+>>> w2 = Wrapper.create_from_decl(decl2)
+>>> w2.blocks
+[{'block': 'nav'}]
+
+>>> decl3 = ('span12', {'block': 'nav'}, {'block': 'content'})
+>>> w3 = Wrapper.create_from_decl(decl3)
+>>> w3.blocks
+[{'block': 'nav'}, {'block': 'content'}]
+
+
+Testing invalid declaration:
+
+>>> Wrapper.create_from_decl([{}, {'block': 'some_block'},])
+Traceback (most recent call last):
+...
+TypeError: Invalid class_ type: <type 'dict'> (expected: basestring)
 
 """
 
@@ -24,15 +43,21 @@ class Block(object): # Block objects are made of dicts :)
         self.name = name
 
 class Wrapper(object): # Wrapper objects are made of tuples
-    def __init__(self, class_):
+    def __init__(self, class_, blocks=None):    
+        if not isinstance(class_, basestring):
+            raise TypeError('Invalid class_ type: %s (expected: basestring)' % type(class_))
+
+        if blocks is not None and not isinstance(blocks, (list, tuple)):
+            raise TypeError('Invalid blocks type: %s (expected: list or tuple)' % type(class_))
+
         self.class_ = class_
+        self.blocks = blocks or []
         
     @classmethod
     def create_from_decl(cls, iterable):
         head = iterable[0]
-        tail = iterable[1:]
-        return Wrapper(head)
-
+        tail = list(iterable[1:])
+        return Wrapper(head, blocks=tail)
 
 
 if __name__ == '__main__':
